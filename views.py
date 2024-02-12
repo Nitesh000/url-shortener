499e7c572327a9dd6ab03cc660a70a358545ac1e
Nitesh000 <ntudu040@gmail.com>
2024-02-12 11:54:57 +0530

    feat: added forms and db methods
    - finish form
      - use the django forms to quicky use the forms
      - use the crispy package for styling the form
      - with the crispy package use the tailwind
      - added functionality to it
    - done with the remainng things

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import HttpResponse, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 
 from .models import Link
+from .forms import LinkForm
 
 
 # Create your views here.
@@ -9,3 +10,27 @@ def index(request):
     links = Link.objects.all()
     context = {"links": links}
     return render(request, "links/index.html", context)
+
+
+# shortened url -> original url (destination)
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click()
+
+    return redirect(link.url)
+
+
+def add_link(request):
+    if request.method == "POST":
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            # save the data and return the user to home page
+            form.save()
+            return redirect(reverse("home"))
+    else:
+        form = LinkForm()
+    context = {
+        "form": form,
+    }
+
+    return render(request, "links/create.html", context)
